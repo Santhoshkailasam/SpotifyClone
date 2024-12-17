fbddb4111851fb59dc9bdae81ed333ce55be039d
Santhoshkailasam <kailasam5107@gmail.com>
2024-12-17 18:37:19 +0530

    Successfully committed

--- Components/footercomponent.js
+++ Components/footercomponent.js
@@ -2,20 +2,27 @@ import { Text,View,StyleSheet,TextInput,Image } from "react-native";
 import Homeicon from "../assets/spotify/icon/Home_icon_filled.svg"
 import Searchicon from "../assets/spotify/icon/Search_icon.svg"
 import Libraryicon from "../assets/spotify/icon/Library-small.svg"
+import { TouchableOpacity } from "react-native";
 const Footer=()=>{
     return(
         <View style={styles.container}> 
         <View>
+        <TouchableOpacity>
         <Homeicon style={{marginLeft:30,}}></Homeicon>
         <Text style={{color:"white",marginLeft:20}}>Home</Text>
+        </TouchableOpacity>
         </View>
         <View>
+        <TouchableOpacity>
         <Searchicon style={{marginLeft:100,}}></Searchicon>
         <Text style={{color:"#777777",marginLeft:92}}>Search</Text>
+        </TouchableOpacity>
         </View>
         <View>
+        <TouchableOpacity>
         <Libraryicon style={{marginLeft:100,}}></Libraryicon>
         <Text style={{color:"#777777",marginLeft:80}}>Your Library</Text>
+        </TouchableOpacity>
         </View>
         </View>
     )
